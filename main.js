848b2e2a23ce1540403af3ce751d94fec0aa2366
Ron31 <ron3108@gmx.net>
2018-09-19 18:59:19 +0200

    Message Event umlagern

--- main.js
+++ main.js
@@ -23,22 +23,6 @@ client.on("ready", () => {
     client.user.setPresence({status: "online", game:{name: "mit Kartoffeln"}})
 })
 
-client.on("message", msg => {
-    if(msg.content == `<@${client.user.id}>`){
-        msg.channel.send(client.emojis.get('491271120595582978') + " ")
-    }
-
-    if(msg.content.startsWith(client.prefix)){
-        let invoke = msg.content.substr(client.prefix.length).split(" ")[0].toLowerCase()
-        let args = msg.content.substr(client.prefix.length + invoke.length).split(" ")
-
-        if(client.commands.get(invoke)) {
-            client.commands.get(invoke)(msg, args, client)
-        }
-    }
-});
-
-
 async function getFiles(dir) {
     const subdirs = await fs.readdirSync(dir);
     const files = await Promise.all(subdirs.map(async (subdir) => {
