af213a5a9de81bcc0b1e7ff33ddd9b7423f7f853
Ron31 <ron3108@gmx.net>
2018-09-19 18:56:00 +0200

    EventHandler in Main

--- main.js
+++ main.js
@@ -57,6 +57,16 @@ async function load() {
             client.commands.set(basename(item).slice(0, -3), require(`${item}`));
         }
     }
+    fs.readdir("./events/", (err, files) => {
+        if (err) return console.error(err);
+        files.forEach(file => {
+            if (!file.endsWith(".js")) return;
+            const event = require(`./events/${file}`);
+            let eventName = file.split(".")[0];
+            client.on(eventName, event.bind(null, client));
+            delete require.cache[require.resolve(`./events/${file}`)];
+    });
+  });
     console.log(client.commands)
 }  
 
