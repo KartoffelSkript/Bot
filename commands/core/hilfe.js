93ad8a6b74cdf4522be587c9986476d518a9638d
Ron31 <ron3108@gmx.net>
2018-09-19 19:41:02 +0200

    Enabled status, "level", description

--- commands/core/hilfe.js
+++ commands/core/hilfe.js
@@ -1,4 +1,4 @@
-module.exports = (msg, args, client) => {
+module.exports.run = (msg, args, client) => {
         let contents = []
         client.commands.forEach((e, k) => {
             contents.push([k, client.prefix + k])
@@ -7,4 +7,19 @@ module.exports = (msg, args, client) => {
         client.embed.uni(msg, "Hallo! Ich bin der Kartoffel-Mann!", "Ich bin der Offizielle Bot für die Kartoffelskript-Sprache!\nHier siehst Du eine Liste aller Kommandos, die ich beherrsche:", 
         contents,
          "0xffaa00")
-    }
+}
+
+async function getFiles(dir) {
+    const subdirs = await fs.readdirSync(dir);
+    const files = await Promise.all(subdirs.map(async (subdir) => {
+      const res = resolve(dir, subdir);
+      return (await fs.statSync(res)).isDirectory() ? getFiles(res) : res;
+    }));
+    return files.reduce((a, f) => a.concat(f), []);
+}
+
+module.exports.info = {
+    beschreibung: "Hilfe!",
+    level: 0,
+    enabled: true
+}
--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-module.exports = (client, msg) => {
+module.exports = async (client, msg) => {
     if(msg.content == `<@${client.user.id}>`){
         msg.channel.send(client.emojis.get('491271120595582978') + " ")
     }
@@ -6,9 +6,11 @@ module.exports = (client, msg) => {
     if(msg.content.startsWith(client.prefix)){
         let invoke = msg.content.substr(client.prefix.length).split(" ")[0].toLowerCase()
         let args = msg.content.substr(client.prefix.length + invoke.length).split(" ")
-
         if(client.commands.get(invoke)) {
-            client.commands.get(invoke)(msg, args, client)
+            if(client.command.get(invoke).info.enabled != true) return;
+            let level = /*await DB.level und so ein schitt*/ 999
+            if(client.command.get(invoke).info.level > level) return;
+            client.commands.get(invoke).run(msg, args, client);
         }
     }
 }
